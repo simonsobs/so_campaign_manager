cd48286bfed9785ca9375870d69eac0952a1d84a
Ioannis Paraskevakos <iparask@princeton.edu>
2025-06-16 16:06:53 -0400

    adding tests

---
+++ tests/conftest.py
@@ -0,0 +1,50 @@
+import pytest
+from pathlib import Path
+import shutil
+from collections import namedtuple
+
+
+TomlReturn = namedtuple("TomlReturn", ["toml"])
+
+
+@pytest.fixture
+def simple_toml(tmp_path):
+    d = tmp_path
+    p = d / "campaign.toml"
+    p.write_text(
+        """
+        [campaign.ml-mapmaking]
+        context = "context.yaml"
+        area = "so_geometry_v20250306_lat_f090.fits"
+        output_dir = "output"
+        bands = "f090"
+        wafer = "ws0"
+        comps = "TQU"
+        maxiter = 10
+        query = "obs_id='1575600533.1575611468.ar5_1'"
+        tiled = 1
+        site = "act"
+        [campaign.ml-mapmaking.environment]
+        MOBY2_TOD_STAGING_PATH = "/tmp/"
+        DOT_MOBY2 = "act_dot_moby2"
+        SOTODLIB_SITECONFIG = "site.yaml"
+    """
+    )
+    return TomlReturn(p)
+
+@pytest.fixture
+def lite_toml(tmp_path):
+    d = tmp_path
+    p = d / "campaign.toml"
+    p.write_text(
+        """
+        [campaign.ml-mapmaking]
+
+        context = "context.yaml"
+        area = "so_geometry_v20250306_lat_f090.fits"
+        output_dir = "output"
+        query = "obs_id='1575600533.1575611468.ar5_1'"
+        site = "act"
+    """
+    )
+    return TomlReturn(p)
---
+++ tests/test_workflows.py
@@ -0,0 +1,45 @@
+import pytest
+import toml
+from socm.workflows import MLMapmakingWorkflow
+
+
+def test_mlworkflow(simple_toml):
+
+    config = toml.load(simple_toml.toml)
+    workflow = MLMapmakingWorkflow(**config["campaign"]["ml-mapmaking"])
+    assert workflow.context == "context.yaml"
+    assert workflow.area == "so_geometry_v20250306_lat_f090.fits"
+    assert workflow.output_dir == "output"
+    assert workflow.bands == "f090"
+    assert workflow.wafer == "ws0"
+    assert workflow.comps == "TQU"
+    assert workflow.maxiter == 10
+    assert workflow.query == "obs_id='1575600533.1575611468.ar5_1'"
+    assert workflow.tiled == 1
+    assert workflow.site == "act"
+    assert workflow.environment["MOBY2_TOD_STAGING_PATH"] == "/tmp/"
+    assert workflow.environment["DOT_MOBY2"] == "act_dot_moby2"
+    assert workflow.environment["SOTODLIB_SITECONFIG"] == "site.yaml"
+    assert workflow.name == "ml_mapmaking_workflow"
+    assert workflow.executable == "so-site-pipeline"
+    assert workflow.subcommand == "make-ml-map"
+    assert workflow.id is None  # Default value for id is None
+
+
+def test_get_arguments(simple_toml):
+    config = toml.load(simple_toml.toml)
+    workflow = MLMapmakingWorkflow(**config["campaign"]["ml-mapmaking"])
+    arguments = workflow.get_arguments()
+    assert (
+        arguments
+        == "obs_id='1575600533.1575611468.ar5_1' so_geometry_v20250306_lat_f090.fits output --bands=f090 --comps=TQU --context=context.yaml --maxiter=10 --site=act --tiled=1 --wafer=ws0"
+    )
+
+
+def test_get_command(lite_toml):
+
+    config = toml.load(lite_toml.toml)
+    workflow = MLMapmakingWorkflow(**config["campaign"]["ml-mapmaking"])
+    command = workflow.get_command(ranks=2)
+    expected = "srun --cpu_bind=cores --export=ALL --ntasks-per-node=2 --cpus-per-task=8 so-site-pipeline make-ml-map obs_id='1575600533.1575611468.ar5_1' so_geometry_v20250306_lat_f090.fits output --context=context.yaml --site=act"
+    assert command == expected
