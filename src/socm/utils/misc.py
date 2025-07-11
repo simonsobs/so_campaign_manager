be1ee84f8d24b4fac6157301bb1710ea1703b3f8
Ioannis Paraskevakos <iparask@princeton.edu>
2025-07-11 14:23:13 -0400

    removing unused functions

--- src/socm/utils/misc.py
+++ src/socm/utils/misc.py
@@ -1,21 +1,3 @@
-from uuid import uuid4
-
-
-def generate_ids(namespace: str) -> str:
-    return namespace + uuid4().hex[:8]
-
-
-# def dict_factory(cursor: Cursor, row: Tuple) -> Dict:
-#     d = {}
-#     for idx, col in enumerate(cursor.description):
-#         d[col[0]] = row[idx]
-#     return d
-
-
-# def linear_func(x: float, a: float, b: float) -> float:
-#     return a * x + b
-
-
 def get_workflow_entries(campaign_dict, subcampaign_map=None):
     """
     Extract workflow entries from a campaign dictionary using a predefined mapping.
