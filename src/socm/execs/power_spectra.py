626e9397398a42645cdd0a78ba2297fdb02677d8
Ioannis Paraskevakos <iparask@princeton.edu>
2026-04-03 12:10:55 -0400

    fix:base path entry corrected

--- src/socm/execs/power_spectra.py
+++ src/socm/execs/power_spectra.py
@@ -65,6 +65,13 @@ def _main(args: Namespace) -> None:
                 humanfriendly.parse_timespan(workflow_config["resources"]["runtime"])
                 / 60
             )  # in minutes
+
+        workflow_base_path = os.getcwd()
+        if "base-path" in workflow_config:
+            workflow_base_path = workflow_config["base-path"]
+        elif "base-path" in config["campaign"]:
+            workflow_base_path = config["campaign"]["base-path"]
+
         workflow_dict = {"name": workflow_name,
                          "id": last_workflow_id,
                          "executable": workflow_config['executable'],
@@ -73,7 +80,7 @@ def _main(args: Namespace) -> None:
                          "resources": workflow_config["resources"],
                          "script_args": workflow_config.get('script-args', []),
                          "script_flags": workflow_config.get('script-flags', []),
-                         "base_path": workflow_config.get('base-path', os.getcwd())
+                         "base_path": workflow_base_path
                          }
         for arg_name, arg_value in workflow_config.get('script-kwargs', {}).items():
             workflow_dict[arg_name] = arg_value
